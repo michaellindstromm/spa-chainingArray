4ad0368ba572f9b65aa051e13a5183362a7cebf1
Michael Lindstrom <michaellindstr@gmail.com>
2017-07-20 15:32:33 -0500

    Realized I didn't correctly complete last task so I wrapped everything in a console log

--- chaining.js
+++ chaining.js
@@ -1,12 +1,12 @@
 let integers = [13, 25, 6, 3, 11, 2, 18, 7, 21, 1, 29, 20, 12, 8];
 
-let singleFunc = () => integers.sort(function(a,b){return b-a;}).filter(function(e){ console.log();return e<19;}).map(function(a){return (a * 1.5)-1}).reduce(function(p,c,i,a){return p + c})
+console.log(integers.sort(function(a,b){return b-a;}).filter(function(e){return e<19;}).map(function(a){return (a * 1.5)-1}).reduce(function(p,c,i,a){return p + c}));
 
 
 
 
 
 
-console.log(singleFunc());
+
 
 // filter(function(e){return e<19;}).forEach(function(num){(num*1.5)-1})
